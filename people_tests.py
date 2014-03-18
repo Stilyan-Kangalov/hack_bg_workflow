c4d38f5d364a3cb6b94920049529dfa8f8563056
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-03-18 16:08:52 +0200

    Person Class

--- people_tests.py
+++ people_tests.py
@@ -1,10 +1,12 @@
 import unittest
 import people
+import os
 
 class people_test(unittest.TestCase):
     """docstring for people_test"""
     def setUp(self):
         self.new_person = people.People("Pesho", 0)
+        # self.file = open('person.txt','a+')
 
     def test_input_name(self):
         self.assertEqual("Pesho", self.new_person.get_name())
@@ -13,11 +15,21 @@ class people_test(unittest.TestCase):
         self.assertEqual(True, self.new_person.push_in_person())
 
     def test_id(self):
-        reuslt = new_person.count_rows()
-        self.assertEqual(result + 1, self.new_person.get_id())
+        result = self.new_person.count_rows()
+        self.new_person.push_in_person()
+        self.assertEqual(result, self.new_person.get_id())
 
     def test_person_txt_file(self):
-        file = open(people.txt,'a+')
+        result = self.new_person.push_in_person()
+        self.assertEqual(True, result)
+        file = open('people.txt','r')
         content = file.read()
+        file.close()
         self.assertEqual("1.Pesho",content)
 
+    def tearDown(self):
+        # self.file.close()
+        os.remove('person.txt')
+
+if __name__ == '__main__':
+    unittest.main()
