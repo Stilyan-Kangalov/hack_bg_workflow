06d4c6f7a1301cb39a1632e1d41c8726a41c9b2b
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2014-03-20 02:53:04 +0200

    Some updates

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 
 #from people import *
 #from mails import *
+from sys import exit
 
 # Define main section with functions
 
@@ -17,6 +18,7 @@ def is_command(fixed_command, command_string):
 # Set-up some empty vars + "password vars" for identification:
 control = []
 register_box = {}
+files_list = []
 code_register = 3015
 code_save = 1045
 
@@ -25,29 +27,54 @@ code_save = 1045
 # Set-up the base functions 
 
 def run_unknown_command():
-	command_error = ["Error: Unknown command!",
-                       "Try one of the following:",
-                       " register <name> <e-mail>",
-                       " unique <e-mail>",
-                       " search",
-                       " merge",
-                       " export",
-                       " finish"]
+	command_error = ["Here is a full list of commands:",
+                       "* show_lists - Prints all lists to the screen. Each list is assigned with a unique identifier",
+                       "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
+                       "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
+                       "* create <list_name> - Creates a new empty list, with the given name.",
+                       "* search_email <email> - Performs a search into all lists to see if the given email is present. Shows all lists, where the email was found.",
+                       "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
+                       "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
+                       "* exit - this will quit the program"]
 	for line in command_error:
 		print(line)
 
 
 def run_help(fixed_command):
-	command_help = ["Great, you choose \'help\' command!",
-                       "Try one of the following:",
-                       " register <name> <e-mail>",
-                       " unique <e-mail>",
-                       " search",
-                       " merge",
-                       " export",
-                       " finish"]
+	command_help = ["Here is a full list of commands:",
+                       "* show_lists - Prints all lists to the screen. Each list is assigned with a unique identifier",
+                       "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
+                       "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
+                       "* create <list_name> - Creates a new empty list, with the given name.",
+                       "* search_email <email> - Performs a search into all lists to see if the given email is present. Shows all lists, where the email was found.",
+                       "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
+                       "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
+                       "* exit - this will quit the program"]
 	for line in list(command_help):
 		print(line)	
+
+def update_filelist():	
+
+	with open("all_lists.txt", "a") as f:
+			for l in files_list:
+				bo = files_list.index(l)
+				f.write(str(bo))
+				f.write(" - ")
+				f.write(l)
+				f.write("\n")
+	f.close()				
+
+def run_create(fixed_command):
+	filename = fixed_command[1]
+	files_list.append(filename)
+	target = open (filename, 'w+')
+	#print(files_list)
+	update_filelist()
+	target.close()
+	files_list.clear()
+	print("Your list has been created.")
+
+
 
 def sample():
 	print(register_box)
@@ -93,16 +120,19 @@ def main():
    		print ("If you need tip, enter <help> command:")
    		command = get_command(input("Enter command>")) 
 
-   		if is_command(command, "register"):
+   		if is_command(command, "create"):
+   			run_create(command)
+
+   		elif is_command(command, "add"):
    			run_register(command)
 
-   		elif is_command(command, "unique"):
-   			run_unique(command)
-   		
-   		elif is_command(command, "search"):
-   			run_search(command)
+   		elif is_command(command, "show_lists"):
+   			run_load_lists(command)		
+  		
+   		elif is_command(command, "search_email"):
+   			run_search_in_mails(command)
 
-   		elif is_command(command, "merge"):
+   		elif is_command(command, "merge_lists"):
    			run_merge(command)
 
    		elif is_command(command, "help"):
@@ -111,7 +141,7 @@ def main():
    		elif is_command(command, "export"):
    			run_export(command)
 
-   		elif is_command(command, "finish"):
+   		elif is_command(command, "exit"):
    			run_finish(command)   		
 
    		else:
