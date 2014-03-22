ee1b5942d9a4c8c7e6ca49b945f04e20998a33f7
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-03-22 15:43:59 +0200

    starting new branch

--- index.py
+++ index.py
@@ -26,10 +26,10 @@ code_save = 1045
 
 
 
-# Set-up the base functions 
+# Set-up the base functions
 
 def run_unknown_command():
-	command_error = ["Here is a full list of commands:",
+    command_error = ["Here is a full list of commands:",
                        "* show_lists - Prints all lists to the screen. Each list is assigned with a unique identifier",
                        "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
                        "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
@@ -38,12 +38,12 @@ def run_unknown_command():
                        "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
                        "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
                        "* exit - this will quit the program"]
-	for line in command_error:
-		print(line)
+    for line in command_error:
+        print(line)
 
 
 def run_help(fixed_command):
-	command_help = ["Here is a full list of commands:",
+    command_help = ["Here is a full list of commands:",
                        "* show_lists - Prints all lists to the screen. Each list is assigned with a unique identifier",
                        "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
                        "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
@@ -52,159 +52,160 @@ def run_help(fixed_command):
                        "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
                        "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
                        "* exit - this will quit the program"]
-	for line in list(command_help):
-		print(line)	
+    for line in list(command_help):
+        print(line)
 '''
-def update_filelist():	
+def update_filelist():
 
-	with open("all_lists.txt", "a") as f:
-			for l in files_list:
-				bo = files_list.index(l)
-				f.write(str(bo))
-				f.write(" - ")
-				f.write(l)
-				f.write("\n")
-	f.close()				
+    with open("all_lists.txt", "a") as f:
+            for l in files_list:
+                bo = files_list.index(l)
+                f.write(str(bo))
+                f.write(" - ")
+                f.write(l)
+                f.write("\n")
+    f.close()
 
-def run_create(fixed_command):	
-	filename = fixed_command[1]
-	files_list.append(filename)
-	target = open (filename, 'w+')
-	#print(files_list)
-	update_filelist()
-	target.close()
-	files_list.clear()
-	print("Your list has been created.")
+def run_create(fixed_command):
+    filename = fixed_command[1]
+    files_list.append(filename)
+    target = open (filename, 'w+')
+    #print(files_list)
+    update_filelist()
+    target.close()
+    files_list.clear()
+    print("Your list has been created.")
 '''
 # Define Class:
 
 class MailList(object):
-	"""basic file functions for MailList"""
-	def __init__(self, arg):
-		self.arg = arg
+    """basic file functions for MailList"""
+    def __init__(self, arg):
+        self.arg = arg
 
-	def create_files(self):
-		path = 'data_base/'
-		if not os.path.exists(path):
-			os.makedirs(path)
+    def create_files(self):
+        path = 'data_base/'
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-		filename = self[1] + '.txt'
-		# file = open(filename, 'a+')
-		# file.close()
-		with open(os.path.join(path, filename), 'a+') as temp_file:
-			temp_file.close()
+        filename = self[1] + '.txt'
+        # file = open(filename, 'a+')
+        # file.close()
+        with open(os.path.join(path, filename), 'a+') as temp_file:
+            temp_file.close()
 
-	'''def locate_and_run(self):
-		file_name_list = []
-		for root, dirs, files in os.walk('./data_base'):
-			for file in files:
-				file_name_list.append(file)
-		return file_name_list'''
+    '''def locate_and_run(self):
+        file_name_list = []
+        for root, dirs, files in os.walk('./data_base'):
+            for file in files:
+                file_name_list.append(file)
+        return file_name_list'''
 
-	def show_lists(self):
-		file_name_list = []
-		for root, dirs, files in os.walk('./data_base'):
-			for file in files:
-				file_name_list.append(file)
-		#return file_name_list
+    def show_lists(self):
+        file_name_list = []
+        for root, dirs, files in os.walk('./data_base'):
+            for file in files:
+                file_name_list.append(file)
+        #return file_name_list
 
-		i = 1
-		for item in file_name_list:
-			print(i,'-',item)
-			i+=1
+        i = 1
+        for item in file_name_list:
+            print(i,'-',item)
+            i+=1
 
-	def show_list(self):
-		path = 'data_base/'
-		filename = self[1]
-		with open(os.path.join(path, filename), 'r') as temp_file:
-			contents = temp_file.read()
-			print(contents)
-			temp_file.close()
+    def show_list(self):
+        path = 'data_base/'
+        filename = self[1]
+        with open(os.path.join(path, filename), 'r') as temp_file:
+            contents = temp_file.read()
+            print(contents)
+            temp_file.close()
 
 # for refactioring
-	def adding_information_to_txt_file(file_name, name, mail):
-		path = 'data_base/'
-		filename = file_name + '.txt'
-		with open(os.path.join(path, filename), 'a+') as temp_file:
-			temp_file.close()					
+    def adding_information_to_txt_file(file_name, name, mail):
+        path = 'data_base/'
+        filename = file_name + '.txt'
+        with open(os.path.join(path, filename), 'a+') as temp_file:
+            temp_file.write(str(name) + ' ' + str(mail) + '\n')
+            temp_file.close()
 
-new_list = MailList		
+new_list = MailList
 
 def sample():
-	print(register_box)
+    print(register_box)
 
 def run_register(fixed_command):
-	if len(fixed_command) != 3:
-		print("You can't use it in that way, please try again!")
-	else:
-		print("Taking registration from %s with %s as e-mail adress." % (fixed_command[1], fixed_command[2]))
-		register_box[fixed_command[1]] = fixed_command[2]
-		control.append(code_register)
+    if len(fixed_command) != 3:
+        print("You can't use it in that way, please try again!")
+    else:
+        print("Taking registration from %s with %s as e-mail adress." % (fixed_command[1], fixed_command[2]))
+        register_box[fixed_command[1]] = fixed_command[2]
+        control.append(code_register)
 
-		if len(register_box) != 0:
-			while True:
-				n = input("Please confirm by entering 'yes' or 'no':")
-				if n.strip() == 'no':
-					print("Your entry was rejected.")
-					register_box.clear()
-					break
-				if n.strip() == 'yes':
-					print("Your entry was saved")
-					sample()
-					break
+        if len(register_box) != 0:
+            while True:
+                n = input("Please confirm by entering 'yes' or 'no':")
+                if n.strip() == 'no':
+                    print("Your entry was rejected.")
+                    register_box.clear()
+                    break
+                if n.strip() == 'yes':
+                    print("Your entry was saved")
+                    sample()
+                    break
 
 
 def run_finish(fixed_command):
-	if code_save not in control:		
-		msg = ["You haven't made any registration.",
-		            "If you wish to exit, type <finish> again.",
-		            "If you want to make registration in the system, type <register> <name> <e-mail>."]
+    if code_save not in control:
+        msg = ["You haven't made any registration.",
+                    "If you wish to exit, type <finish> again.",
+                    "If you want to make registration in the system, type <register> <name> <e-mail>."]
 
-		for line in msg:
-			print(line)
+        for line in msg:
+            print(line)
 
-		control.append(code_save)
-	elif code_save in control:
-		exit("Everything clear. Goodbye!")
+        control.append(code_save)
+    elif code_save in control:
+        exit("Everything clear. Goodbye!")
 
 # Set-up the main function:
 
 def main():
-   	while True:
-   		print ("If you need tip, enter <help> command:")
-   		command = get_command(input("Enter command>")) 
+    while True:
+        print ("If you need tip, enter <help> command:")
+        command = get_command(input("Enter command>"))
 
-   		if is_command(command, "create"):
-   			new_list.create_files(command)
+        if is_command(command, "create"):
+            new_list.create_files(command)
 
-   		elif is_command(command, "add"):
-   			new_list.adding_information_to_txt_file(command)
+        elif is_command(command, "add"):
+            new_list.adding_information_to_txt_file(command)
 
-   		elif is_command(command, "show_lists"):
-   			new_list.show_lists(command)
+        elif is_command(command, "show_lists"):
+            new_list.show_lists(command)
 
-   		elif is_command(command, "show_list"):
-   			new_list.show_list(command)			
-  		
-   		elif is_command(command, "search_email"):
-   			run_search_in_mails(command)
+        elif is_command(command, "show_list"):
+            new_list.show_list(command)
 
-   		elif is_command(command, "merge_lists"):
-   			run_merge(command)
+        elif is_command(command, "search_email"):
+            run_search_in_mails(command)
 
-   		elif is_command(command, "help"):
-   			run_help(command)	
+        elif is_command(command, "merge_lists"):
+            run_merge(command)
 
-   		elif is_command(command, "export"):
-   			run_export(command)
+        elif is_command(command, "help"):
+            run_help(command)
 
-   		elif is_command(command, "exit"):
-   			run_finish(command)   		
+        elif is_command(command, "export"):
+            run_export(command)
 
-   		else:
-   			run_unknown_command() 
+        elif is_command(command, "exit"):
+            run_finish(command)
+
+        else:
+            run_unknown_command()
 
 # Run the main function
 
 if __name__ == '__main__':
-    main()            
+    main()
